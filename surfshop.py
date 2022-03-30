f53d17f1525742d832d134b8f8363024b8d5bf22
Mohamed <mmuseaden@gmail.com>
2022-03-30 18:28:47 +0100

    first commit

---
+++ surfshop.py
@@ -0,0 +1,54 @@
+# https://www.codecademy.com/courses/learn-intermediate-python-3/projects/int-python-sams-surf-shop
+import datetime
+
+class TooManyBoardsError(Exception):
+  # Q12
+    def __str__(self):
+      return 'Cart cannot have more than 4 surfboards in it!'
+
+class CheckoutDateError(Exception):
+    # Q13
+    def __str__(self):
+      return 'No dates in the past allowed, select a date in the future!'
+
+class ShoppingCart:
+    def __init__(self):
+        self.num_surfboards = 0
+        self.checkout_date = None
+        self.locals_discount = False
+
+    def add_surfboards(self, quantity=1):
+      # should delete this condition of more than 4
+        if self.num_surfboards + quantity > 4:
+            raise TooManyBoardsError
+        else:
+            self.num_surfboards += quantity
+            suffix = '' if quantity == 1 else 's'
+            return f'Successfully added {quantity} surfboard{suffix} to cart!'
+
+    def set_checkout_date(self, date):
+        if date <= datetime.datetime.now():
+            raise CheckoutDateError
+        else:
+            self.checkout_date = date
+            return date
+
+    def apply_locals_discount(self):
+      # Q11 TO SET locals_discount = True
+      self.locals_discount = True
+
+
+# Q12 testing  
+# t = ShoppingCart()
+# t5 = t.add_surfboards(5)
+# print(t5)
+
+# Q13 testing
+past = datetime.datetime(2020, 12, 3, 10, 20, 59)
+# print(datetime.datetime.now())
+# t = ShoppingCart()
+# t_past = t.set_checkout_date(past)
+# print(t_past)
+
+# DOCS! for datetime
+# A combination of a date and a time. Attributes: year, month, day, hour, minute, second, microsecond, and tzinfo.
---
+++ tests.py
@@ -0,0 +1,56 @@
+# Write your code below: Q1
+import unittest
+import surfshop
+# Q13
+import datetime
+
+# Q2
+class SurfShopTests(unittest.TestCase):
+  # Q3
+  def setUp(self):
+    self.cart = surfshop.ShoppingCart()
+  # Q4
+  def test_add_surfboard(self):
+    # print(self.cart.add_surfboards(1))
+    self.assertEqual(self.cart.add_surfboards(1), 'Successfully added 1 surfboard to cart!')
+  # Q5 Q6
+  def test_add_surfboards(self):
+    # Q10 loop with subTest although we done it before
+    for i in range(2, 10):
+      with self.subTest(i):
+        if i < 5:
+          message = self.cart.add_surfboards(i)
+          self.assertEqual(message, 'Successfully added {i} surfboards to cart!'.format(i=i))
+        else:
+          # Q9 is SKIP but before we ran it
+          self.skipTest("No more limit to a max of 4 people")
+          print(i) #NOTHING RUNS HERE CUZ ALL OF SKIP!!
+          self.assertRaises(surfshop.TooManyBoardsError, self.cart.add_surfboards, i) # i is 5 or more up to 9atm
+      self.cart = surfshop.ShoppingCart() # RESTARTING
+  
+  
+  # Q7 Q8 MARK AS EXPECTED FAILURE
+  # Q11 we commentout expected failure because we fix the error in surfshop.py
+  # @unittest.expectedFailure
+  def test_apply_locals_discount(self):
+      self.cart.apply_locals_discount()
+      self.assertTrue(self.cart.locals_discount)
+
+
+  # Q13
+  def test_checkout_date_today(self):
+    today = datetime.datetime.now()
+    self.assertRaises(surfshop.CheckoutDateError, self.cart.set_checkout_date, today)
+
+  def test_checkout_date_before_today(self):
+    last_year = datetime.datetime(2020, 12, 3, 10, 20, 59)
+    self.assertRaises(surfshop.CheckoutDateError, self.cart.set_checkout_date, last_year)
+  
+  def test_checkout_date_in_the_future(self):
+    time_to_come = datetime.datetime(2023, 1, 27, 14, 30,20)
+    checkout = self.cart.set_checkout_date(time_to_come)
+    self.assertEqual(checkout, self.cart.checkout_date)
+
+
+# Q8 
+unittest.main()
